6bbc0d777de2d63edc8fea855af7fcea1dae9352
spreedated <21370839+spreedated@users.noreply.github.com>
2023-09-27 15:30:13 +0200

    added constants

---
+++ TasmotaQuery/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace TasmotaQuery
+{
+    internal static class Constants
+    {
+        public const string POWER_ON = "power1%20true";
+        public const string POWER_OFF = "power1%20false";
+        public const string SHUTTER_SET_POSITION = "ShutterPosition1%20";
+    }
+}
