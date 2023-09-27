f9ed7b60f5afd306dbbde022d136a0e3ee1b94ae
spreedated <21370839+spreedated@users.noreply.github.com>
2023-09-27 15:30:43 +0200

    added json attributes

--- TasmotaQuery/Device.cs
+++ TasmotaQuery/Device.cs
@@ -1,18 +1,44 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using TasmotaQuery.Models;
+using static TasmotaQuery.Constants;
 
 namespace TasmotaQuery
 {
     public class Device : IDevice
     {
         internal HttpMessageHandler httpHandler;
+
+        [JsonProperty("address")]
         public IPAddress Address { get; set; }
+
+        [JsonIgnore()]
         public bool IsAvailable { get; internal set; }
+
+        [JsonProperty("deviceproperties")]
         public DeviceProperties DeviceProperties { get; } = new();
+
+        [JsonProperty("devicestatusresponses")]
         public DeviceStatusResponses DeviceStatusResponses { get; } = new();
+
+        [JsonIgnore()]
+        public int ShutterPosition { get; set; }
+        [JsonIgnore()]
+        public bool ShutterRunning { get; set; }
+        [JsonIgnore()]
+        public int ShutterDirection { get; set; }
+        public enum Powerstates
+        {
+            Unknown = 0,
+            On,
+            Off
+        }
+        [JsonIgnore()]
+        public Powerstates Powerstate { get; set; }
 
         #region Constructor
         public Device(string address)
@@ -49,6 +75,33 @@ namespace TasmotaQuery
         public IQuery Query()
         { 
             return new Query(this);
+        }
+
+        public async Task SetPower(bool on = true)
+        {
+            using (HttpClient hc = new())
+            {
+                await hc.GetAsync($"http://{this.Address}/cm?cmnd={(on ? POWER_ON : POWER_OFF)}");
+            }
+
+            await this.Query().GetStatus();
+        }
+
+        public async Task SetShutter(int position)
+        {
+            if (this.ShutterRunning)
+            {
+                return;
+            }
+
+            this.ShutterRunning = true;
+
+            using (HttpClient hc = new())
+            {
+                await hc.GetAsync($"http://{this.Address}/cm?cmnd={SHUTTER_SET_POSITION}{position}");
+            }
+
+            this.ShutterRunning = false;
         }
     }
 }
--- TasmotaQuery/Models/DeviceStatusResponses.cs
+++ TasmotaQuery/Models/DeviceStatusResponses.cs
@@ -1,11 +1,18 @@
-﻿namespace TasmotaQuery.Models
+﻿using Newtonsoft.Json;
+
+namespace TasmotaQuery.Models
 {
     public sealed class DeviceStatusResponses
     {
+        [JsonProperty("state")]
         public State State { get; internal set; }
+        [JsonProperty("status")]
         public Status Status { get; internal set; }
+        [JsonProperty("firmware")]
         public Firmware Firmware { get; internal set; }
+        [JsonProperty("time")]
         public Time Time { get; internal set; }
+        [JsonProperty("sensors")]
         public Sensors Sensors { get; internal set; }
     }
 }
