90866e617da67aea5e44f6f3877fb4cabfe109f6
spreedated <21370839+spreedated@users.noreply.github.com>
2023-09-27 04:58:10 +0200

    extensions in tasmota layer

---
+++ TasmotaQuery/Json/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace TasmotaQuery.Json
+{
+    internal static class Extensions
+    {
+        internal static bool IsValidJson(this string json)
+        {
+            try
+            {
+                JObject.Parse(json);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
